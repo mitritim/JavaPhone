cea22625b1f45bf3134eab57d3db76fe046b0435
mitritim <mitritim@gmx.com>
2014-01-07 19:16:25 +0100

    + UserList.saveData()
    rename .getUserData() > .getUserById()

--- src/javaphone/UserList.java
+++ src/javaphone/UserList.java
@@ -8,19 +8,24 @@ import java.util.HashMap;
  * @author iGroup
  */
 public class UserList extends ArrayList<HashMap> {
+
     private final DataFileIO userDataFile;
-    
+
     public UserList() {
         userDataFile = new DataFileIO("users.xml", "users", "user");
         this.addAll(userDataFile.read());
     }
 
-    public HashMap getUserData(int userId) {         
-        for(HashMap user : this) {
-            if(user.containsValue(userId)) {
+    public HashMap getUserById(int userId) {
+        for (HashMap user : this) {
+            if (userId == (int) user.get("userId")) {
                 return user;
             }
         }
         return null;
+    }
+
+    public boolean saveData() {
+        return userDataFile.write(this);
     }
 }
