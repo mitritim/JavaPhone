0ed4ec2dac8d9f99b382fe805750919ac012d609
mariahedstrand <mariahedstrand@gmail.com>
2014-01-10 21:41:23 +0100

    Update ServiceList.java
    Fixade till hashmap
    tog bort onödiga delar
    Adderade javadoc

--- src/javaphone/ServiceList.java
+++ src/javaphone/ServiceList.java
@@ -4,23 +4,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- *
+ * This class handles the service information when 
+ * searching for the service id number.
+ * 
  * @author iGroup
  */
 public class ServiceList extends ArrayList<HashMap> {
+    
     private final DataFileIO serviceDataFile;
     
+    /**
+     * Adds service to the datafile.
+     */
     public ServiceList() {
         serviceDataFile = new DataFileIO("services.xml", "services", "service");
         this.addAll(serviceDataFile.read());
     }
 
-    public double getServicePrice(Service service) {
-        return 0;
-    }
-
-    public boolean setServicePrice(Service service, double price) {
-        return false;
-    }
-
+    /**
+     * Searches for service with specific serviceId
+     * @param serviceId
+     * @return service data as HashMap if service found 
+     *         null if service not found
+     */  
+    public HashMap getServiceById(int serviceId) {
+        for (HashMap service : this) {
+            if (serviceId == (int) service.get("serviceId")) {
+                return service;
+            }
+        }
+        return null;
+    }    
 }
