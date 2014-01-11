bae1fd754f0341bc8e838b16fba3c62ea0153044
mitritim <mitritim@gmx.com>
2014-01-11 18:48:32 +0100

    Update CustomerList.java

--- src/javaphone/CustomerList.java
+++ src/javaphone/CustomerList.java
@@ -1,20 +1,22 @@
 package javaphone;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * This class handles the customers information when 
- * searching for the customer id number.
- * 
+ * This class holds a list of all customers and provides methods to access it.
+ *
  * @author iGroup
  */
 public class CustomerList extends ArrayList<HashMap> {
 
     private final DataFileIO customerDataFile;
-    
+
     /**
-     * Adds customer to the datafile.
+     * Constructor for the class CustomerList. <br />
+     * First creates an instance of DataFileIO <br />
+     * and adds reference to HashMap objects it gets from an xml-file.
      */
     public CustomerList() {
         customerDataFile = new DataFileIO("customers.xml", "customers", "customer");
@@ -23,11 +25,12 @@ public class CustomerList extends ArrayList<HashMap> {
 
     /**
      * Searches for customer with specific customerId
+     *
      * @param customerId
-     * @return customer data as HashMap if customer found 
-     *         null if customer not found
-     */     
-    public HashMap getServiceById(int customerId) {
+     * @return customer data as HashMap if customer found null if customer not
+     * found
+     */
+    public HashMap getCustomerById(int customerId) {
         for (HashMap customer : this) {
             if (customerId == (int) customer.get("customerId")) {
                 return customer;
@@ -38,25 +41,65 @@ public class CustomerList extends ArrayList<HashMap> {
 
     /**
      * Gets the number of customers by service.
-     * @param serviceId
-     * @return customer number
+     *
+     * @param   serviceId
+     * @return  The number of customers with the specified service.
      */
     public int getCustomerCountByService(int serviceId) {
-        return 0;
-    }
-    
-    /**
-     * Prepares the report
-     */
-    public void prepareReport() {
-    }
-    
-    /**
-     * Gets the next contract
-     * @return null
-     */
-    public String getNextContract() {
-        return null;
+        int customerCount = 0;
+        for (HashMap hashMap : this) {
+            if ((int) hashMap.get("serviceId") == serviceId) {
+                customerCount++;
+            }
+        }
+        return customerCount;
     }
 
+    /**
+     * Gets the number of customers registered by a user.
+     *
+     * @param   userId
+     * @return  The number of customers registered by a user.
+     */
+    public int getCustomerCountByUser(int userId) {
+        int customerCount = 0;
+        for (HashMap hashMap : this) {
+            if ((int) hashMap.get("userId") == userId) {
+                customerCount++;
+            }
+        }
+        return customerCount;
+    }
+
+    /**
+     * Gets a list of files in a folder with a specific extension.
+     *
+     * @param folderName Folder to search for files. Use "." for root.
+     * @param validExtension Valid file extension.
+     * @return ArayList with relative file-paths as Strings.
+     */
+    public ArrayList getFileList(String folderName, String validExtension) {
+        ArrayList files = new ArrayList();
+        File folder = new File(folderName); // the folder to check for files
+        File[] folderContent = folder.listFiles(); //a list of all files and folders within the chosen folder
+
+        for (File file : folderContent) {
+            String fileName = file.toString();
+            String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+            if (file.isFile() & fileExtension.equalsIgnoreCase(validExtension)) {
+                // Checks that it is not a folder or a file of another type.
+                files.add(fileName);
+            }
+        }
+        return files;
+    }
+
+    /**
+     * Saves data to xml file.
+     *
+     * @return true if data was saved false if data was not saved
+     */
+    public boolean saveData() {
+        return customerDataFile.write(this);
+    }
 }
